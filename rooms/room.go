66e0d9e410d6b3a63f380e64d768a48774362539
Anatolii Kurotych <akurotych@gmail.com>
2019-07-27 18:32:41 +0300

    Refactor clientlist, chatroom, broadcaster (#18)

--- rooms/room.go
+++ rooms/room.go
@@ -1,32 +1,47 @@
 package rooms
 
 import (
+	"sync"
+
 	"github.com/gorilla/websocket"
 	"github.com/gospeak/chat-engine/clients"
+	bc "github.com/gospeak/chat-engine/rooms/broadcaster"
 )
 
 type GeneralChatRoom struct {
+	broadcaster *bc.Broadcaster
+	mutex       *sync.RWMutex
 	clients     *clients.ClientList
-	broadcaster *clients.Broadcaster
 }
 
 func NewGeneralChatRoom() *GeneralChatRoom {
 	gcr := &GeneralChatRoom{
-		clients: &clients.ClientList{},
+		clients: clients.NewChatClientList(),
+		mutex:   &sync.RWMutex{},
 	}
-	gcr.broadcaster = clients.NewBroadcaster(gcr.clients)
+	gcr.broadcaster = bc.NewBroadcaster(gcr.clients, gcr.mutex)
 	return gcr
 }
 
+// You should think about close websocket.Conn if this function return false
 func (gcr *GeneralChatRoom) AddClient(conn *websocket.Conn, sessionID string) bool {
+	gcr.mutex.Lock()
+	defer gcr.mutex.Unlock()
+	if gcr.clients.IsExists(sessionID) {
+		return false
+	}
 	client := clients.NewChatClient(conn, sessionID, gcr.broadcaster.ReadCh)
 	return gcr.clients.Add(*client)
 }
 
+// Attention! Quantity items in map can be changed after return value
+// Race condition is possible
 func (gcr *GeneralChatRoom) CountClients() uint32 {
 	return gcr.clients.Count()
 }
 
 func (gcr *GeneralChatRoom) RemoveClientBySessionID(ID string) bool {
+	gcr.mutex.Lock()
+	defer gcr.mutex.Unlock()
 	return gcr.clients.RemoveBySessionID(ID)
 }
--- rooms/room_test.go
+++ rooms/room_test.go
@@ -1,1 +1,73 @@
 package rooms_test
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/gospeak/chat-engine/rooms"
+	"github.com/stretchr/testify/assert"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func servWs(w http.ResponseWriter, r *http.Request) {
+	_, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createMockWs() *websocket.Conn {
+	s := httptest.NewServer(http.HandlerFunc(servWs))
+	defer s.Close()
+	// Convert http://127.0.0.1 to ws://127.0.0.
+	u := strings.ReplaceAll(s.URL, "http", "ws")
+
+	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+	return ws
+}
+
+func TestInterface(t *testing.T) {
+	testFun := func(cr rooms.ChatRoom) {}
+	cr := rooms.NewGeneralChatRoom()
+	testFun(cr)
+}
+
+func TestAddClient(t *testing.T) {
+	ws := createMockWs()
+	defer ws.Close()
+	cr := rooms.NewGeneralChatRoom()
+	assert.True(t, cr.AddClient(ws, "ID"))
+	assert.False(t, cr.AddClient(ws, "ID"), "Add client with same id must be failed")
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, cr.CountClients(), uint32(1))
+	assert.True(t, cr.RemoveClientBySessionID("ID"))
+	assert.Equal(t, cr.CountClients(), uint32(0))
+}
+
+func TestRemoveClient(t *testing.T) {
+	ws := createMockWs()
+	ws2 := createMockWs()
+	defer ws.Close()
+	defer ws2.Close()
+
+	cr := rooms.NewGeneralChatRoom()
+	assert.True(t, cr.AddClient(ws, "ID"))
+	assert.True(t, cr.AddClient(ws2, "ID2"))
+	assert.False(t, cr.AddClient(ws, "ID"), "Add client with same id must be failed")
+	assert.Equal(t, cr.CountClients(), uint32(2))
+	assert.True(t, cr.RemoveClientBySessionID("ID"))
+	assert.Equal(t, cr.CountClients(), uint32(1))
+	assert.False(t, cr.RemoveClientBySessionID("ID"))
+	assert.Equal(t, cr.CountClients(), uint32(1))
+}
